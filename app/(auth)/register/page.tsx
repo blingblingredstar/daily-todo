fc3a58d6e225f01db08ca1b6322d511f535b8370
yong.liu <564447023@qq.com>
2023-03-27 21:55:45 +0800

    feat: add base routes

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const RegisterPage: FC = () => {
+  return <div>Register</div>;
+};
+
+export default RegisterPage;
---
+++ app/(dashboard)/project/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const ProjectPage: FC = () => {
+  return <div>Project</div>;
+};
+
+export default ProjectPage;
