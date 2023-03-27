205760226303a136e5cfe2b962bac808c7be9745
yong.liu <564447023@qq.com>
2023-03-27 22:15:11 +0800

    feat: remove default layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,10 @@
 import { FC, PropsWithChildren } from 'react';
 
+export const metadata = {
+  title: 'Sign in',
+  description: 'Sign in the app',
+};
+
 const Layout: FC<PropsWithChildren> = ({ children }) => {
   return <div>{children}</div>;
 };
